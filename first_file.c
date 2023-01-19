da7a3ea0edcc68ee07169db01dfb12b2d1e0f99e
anviks <anviks@ttu.ee>
2023-01-19 21:37:53 +0200

    Weird stuff

--- first_file.c
+++ first_file.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
+
 int main() {
-    printf("Hi");
+    char arr[] = "something weird";
+    int some[] = {1, 5, 8};
+    printf("\n%d", some[6]);
+    printf("Hi\n");
+    printf("%s", arr);
+
+    for (int i = 0; i < 3; i++) {
+        printf("\n%p", &some[i]);
+    }
     return 0;
 }
